70cff2ab16c8cf0fcf43624a9dcfc9e175a9e821
Jabbe <Jabbe@DESKTOP-IUO6HDH>
2024-04-03 22:16:15 +0500

    Добавьте файлы проекта.

---
+++ WheelOfFortune/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace WheelOfFortune
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.rotationTimer = new System.Windows.Forms.Timer(this.components);
+            this.pictureBoxWheel = new System.Windows.Forms.PictureBox();
+            this.spinButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxWheel)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // rotationTimer
+            // 
+            this.rotationTimer.Interval = 4;
+            this.rotationTimer.Tick += new System.EventHandler(this.rotationTimer_Tick);
+            // 
+            // pictureBoxWheel
+            // 
+            this.pictureBoxWheel.Location = new System.Drawing.Point(9, 18);
+            this.pictureBoxWheel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBoxWheel.Name = "pictureBoxWheel";
+            this.pictureBoxWheel.Size = new System.Drawing.Size(361, 373);
+            this.pictureBoxWheel.TabIndex = 0;
+            this.pictureBoxWheel.TabStop = false;
+            this.pictureBoxWheel.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBoxWheel_Paint);
+            // 
+            // spinButton
+            // 
+            this.spinButton.Location = new System.Drawing.Point(470, 186);
+            this.spinButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.spinButton.Name = "spinButton";
+            this.spinButton.Size = new System.Drawing.Size(80, 30);
+            this.spinButton.TabIndex = 1;
+            this.spinButton.Text = "button1";
+            this.spinButton.UseVisualStyleBackColor = true;
+            this.spinButton.Click += new System.EventHandler(this.spinButton_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.IndianRed;
+            this.pictureBox1.Location = new System.Drawing.Point(182, 364);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(11, 39);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(626, 413);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.spinButton);
+            this.Controls.Add(this.pictureBoxWheel);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxWheel)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer rotationTimer;
+        private System.Windows.Forms.PictureBox pictureBoxWheel;
+        private System.Windows.Forms.Button spinButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ WheelOfFortune/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WheelOfFortune
+{
+    public partial class Form1 : Form
+    {
+        private Random random;
+        private float currentRotationAngle;
+        private float rotationTarget;
+        private const float FullCircleDegrees = 360;
+
+        private const float ColorSegmentDegrees = FullCircleDegrees / 10;
+
+        private const float SpinDegreesStart1 = ColorSegmentDegrees * 0; // редкий // синий
+        private const float SpinDegreesStart2 = ColorSegmentDegrees * 1 + 36; // эпический // фиолетовый
+        private const float SpinDegreesStart3 = ColorSegmentDegrees * 2 + 36; // обычный // зеленый
+        private const float SpinDegreesStart4 = ColorSegmentDegrees * 3; // легендарный // желтый
+
+        private const float SpinDegreesStart5 = ColorSegmentDegrees * 4; // обычный // зеленый
+        private const float SpinDegreesStart6 = ColorSegmentDegrees * 5; // обычный // зеленый
+        private const float SpinDegreesStart7 = ColorSegmentDegrees * 6 + 36; // эпический // фиолетовый
+        private const float SpinDegreesStart8 = ColorSegmentDegrees * 7; // легендарный // желтый
+        private const float SpinDegreesStart9 = ColorSegmentDegrees * 8; // редкий // синий
+        private const float SpinDegreesStart10 = ColorSegmentDegrees * 9; // обычный // зеленый
+
+        public Form1()
+        {
+            InitializeComponent();
+            random = new Random();
+            currentRotationAngle = 0;
+            rotationTarget = 0;
+        }
+
+        private void rotationTimer_Tick(object sender, EventArgs e)
+        {
+            currentRotationAngle += 5; // Adjust the rotation speed
+            if (currentRotationAngle >= rotationTarget)
+            {
+                rotationTimer.Stop();
+                int degrees = random.Next(360); // Randomly choose a degree to stop
+                                                //MessageBox.Show($"You landed on {GetColor(degrees)}!"); // Display the color
+            }
+            pictureBoxWheel.Invalidate();
+        }
+
+        private void pictureBoxWheel_Paint(object sender, PaintEventArgs e)
+        {
+            DrawWheelSectors(e.Graphics, currentRotationAngle);
+        }
+
+        private void DrawWheelSectors(Graphics g, float rotationAngle)
+        {
+            // Define colors and sectors
+            Color[] colors = { Color.Yellow, Color.Blue, Color.Green, Color.Blue, Color.Purple, Color.Green, Color.Yellow, Color.Green, Color.Green, Color.Purple };
+            int numSectors = colors.Length;
+
+            // Calculate sector size
+            float sectorAngle = 360f / numSectors;
+
+            // Draw sectors with rotation
+            for (int i = 0; i < numSectors; i++)
+            {
+                float startAngle = i * sectorAngle + rotationAngle;
+                using (SolidBrush brush = new SolidBrush(colors[i]))
+                {
+                    g.FillPie(brush, pictureBoxWheel.ClientRectangle, startAngle, sectorAngle);
+                }
+            }
+        }
+
+        private void spinButton_Click(object sender, EventArgs e)
+        {
+            // Choose a rotation method based on probabilities
+            int chance = 1; // Default chance
+
+            currentRotationAngle = 0;
+
+            int randomNumber = random.Next(1, 100);
+
+            if (randomNumber >= 90) // 10% 90 to 100
+            {
+                chance = 1;
+                //MessageBox.Show
+            }
+            else if (randomNumber >= 84) // 5% 84 to 89 
+            {
+                chance = 2;
+                //MessageBox.Show
+            }
+            else if (randomNumber == 1) // 1% 
+            {
+                chance = 3;
+                //MessageBox.Show
+            }
+            else
+            {
+                chance = 4; // Def
+                //MessageBox.Show(" Def ");
+            }
+
+            // MessageBox.Show("Random number: " + randomNumber.ToString());
+
+            // Start rotation based on the chosen chance
+            switch (chance)
+            {
+                case 1:
+                    int randomRare = random.Next(1, 3);
+                    if (randomRare == 1)
+                    {
+                        // MessageBox.Show("SpinDegrees1");
+                        StartRotation(SpinDegreesStart1, SpinDegreesStart1 + ColorSegmentDegrees);
+                    }
+                    else
+                    {
+                        // MessageBox.Show("SpinDegrees9");
+                        StartRotation(SpinDegreesStart9, SpinDegreesStart9 + ColorSegmentDegrees);
+                    }
+                    break;
+                case 2:
+                    int randomEpic = random.Next(1, 3);
+                    if (randomEpic == 1)
+                    {
+                        // MessageBox.Show("SpinDegrees2");
+                        StartRotation(SpinDegreesStart2, SpinDegreesStart2 + ColorSegmentDegrees);
+                    }
+                    else
+                    {
+                        // MessageBox.Show("SpinDegrees7");
+                        StartRotation(SpinDegreesStart7, SpinDegreesStart7 + ColorSegmentDegrees);
+                    }
+                    break;
+                case 3:
+                    int randomLegend = random.Next(1, 3);
+                    if (randomLegend == 1)
+                    {
+                        // MessageBox.Show("SpinDegrees4");
+                        StartRotation(SpinDegreesStart4, SpinDegreesStart4 + ColorSegmentDegrees);
+                    }
+                    else
+                    {
+                        // MessageBox.Show("SpinDegrees8");
+                        StartRotation(SpinDegreesStart8, SpinDegreesStart8 + ColorSegmentDegrees);
+                    }
+                    break;
+                    //MessageBox.Show("SpinDegrees3 // желтый");
+                case 4:
+                    int randomCommon = random.Next(1, 5); 
+                    if (randomCommon == 1)
+                    {
+                        // MessageBox.Show("SpinDegrees3");
+                        StartRotation(SpinDegreesStart3, SpinDegreesStart3 + ColorSegmentDegrees);
+                    }
+                    else if (randomCommon == 2)
+                    {
+                        // MessageBox.Show("SpinDegrees5");
+                        StartRotation(SpinDegreesStart5, SpinDegreesStart5 + ColorSegmentDegrees);
+                    }
+                    else if (randomCommon == 3)
+                    {
+                        // MessageBox.Show("SpinDegrees6");
+                        StartRotation(SpinDegreesStart6, SpinDegreesStart6 + ColorSegmentDegrees);
+                    }
+                    else
+                    {
+                        // MessageBox.Show("SpinDegrees10");
+                        StartRotation(SpinDegreesStart10, SpinDegreesStart10 + ColorSegmentDegrees);
+                    }
+
+                    //MessageBox.Show("SpinDegrees4 // зеленый");
+                    break;
+                default:
+                    int randomCommonDef = random.Next(1, 5);
+                    if (randomCommonDef == 1)
+                    {
+                        // MessageBox.Show("SpinDegrees3");
+                        StartRotation(SpinDegreesStart3, SpinDegreesStart3 + ColorSegmentDegrees);
+                    }
+                    else if (randomCommonDef == 2)
+                    {
+                        // MessageBox.Show("SpinDegrees5");
+                        StartRotation(SpinDegreesStart5, SpinDegreesStart5 + ColorSegmentDegrees);
+                    }
+                    else if (randomCommonDef == 3)
+                    {
+                        // MessageBox.Show("SpinDegrees6");
+                        StartRotation(SpinDegreesStart6, SpinDegreesStart6 + ColorSegmentDegrees);
+                    }
+                    else
+                    {
+                        // MessageBox.Show("SpinDegrees10");
+                        StartRotation(SpinDegreesStart10, SpinDegreesStart10 + ColorSegmentDegrees);
+                    }
+                    break;
+            }
+        }
+
+
+        private void StartRotation(float startDegrees, float endDegrees)
+        {
+            float currentDegrees = currentRotationAngle % FullCircleDegrees; // Получить текущее вращение в градусах
+
+            // Calculate remaining degrees considering positive rotation
+            float remainingDegrees = endDegrees - currentDegrees;
+            if (remainingDegrees < 0)
+            {
+                remainingDegrees += FullCircleDegrees;
+            }
+
+            rotationTarget = currentRotationAngle + remainingDegrees; // Рассчитать целевое вращение
+            rotationTimer.Start();
+        }
+
+
+    }
+}
